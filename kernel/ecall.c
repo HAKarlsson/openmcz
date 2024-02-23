4e12ed2d89634bf2ea754ab0a820454f7fe1e2dd
Henrik Karlsson <henrik10@kth.se>
2024-02-23 13:51:47 +0100

    Refactors system calls

--- kernel/ecall.c
+++ kernel/ecall.c
@@ -8,43 +8,39 @@ void ecall_yield()
 
 void ecall_wfi()
 {
-	wfi();
+        uint64_t mie = csrr(mie);
+        while (!(csrr(mip) & mie))
+                wfi();
+        kernel_yield();
 }
 
 void ecall_send(uint64_t ch, uint64_t msg0, uint64_t msg1)
 {
+	channel_t *chan = current->chan_send[ch];
 	if (ch >= current->n_chan_send) {
 		current->regs.a0 = 0;
-		return;
-	}
-
-	channel_t *chan = current->chan_send[ch];
-	if (chan->head - chan->tail == chan->size) {
+	} else if (chan->head - chan->tail == chan->size) {
 		current->regs.a0 = 0;
-		return;
+	} else {
+		chan->buf[chan->head % chan->size] = msg0;
+		chan->buf[(chan->head + 1) % chan->size] = msg1;
+		chan->head += 2;
+		current->regs.a0 = 1;
 	}
-
-	chan->buf[chan->head % chan->size] = msg0;
-	chan->buf[(chan->head + 1) % chan->size] = msg1;
-	chan->head += 2;
-	current->regs.a0 = 1;
 }
 
 void ecall_recv(uint64_t ch)
 {
+	channel_t *chan = current->chan_recv[ch];
 	if (ch >= current->n_chan_recv) {
 		current->regs.a0 = 0;
-		return;
-	}
-
-	channel_t *chan = current->chan_recv[ch];
-	if (chan->tail == chan->head) {
+	} else if (chan->tail == chan->head) {
 		current->regs.a0 = 0;
 		return;
+	} else {
+		current->regs.a1 = chan->buf[chan->tail % chan->size];
+		current->regs.a2 = chan->buf[(chan->tail + 1) % chan->size];
+		chan->tail += 2;
+		current->regs.a0 = 1;
 	}
-
-	current->regs.a1 = chan->buf[chan->tail % chan->size];
-	current->regs.a2 = chan->buf[(chan->tail + 1) % chan->size];
-	chan->tail += 2;
-	current->regs.a0 = 1;
 }
