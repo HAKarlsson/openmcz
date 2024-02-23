1613028211a94693fc83532ee808e423402144e3
Henrik Karlsson <henrik10@kth.se>
2024-02-23 13:52:13 +0100

    Adds dummy messages in exception and interrupt.

--- kernel/exception.c
+++ kernel/exception.c
@@ -1,5 +1,7 @@
 #include "exception.h"
+#include "uart.h"
 
 void exception_handler(uint64_t mcause, uint64_t mepc, uint64_t mtval)
 {
+        uart_puts("EXCEPTION");
 }
--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -1,5 +1,7 @@
 #include "interrupt.h"
+#include "uart.h"
 
 void interrupt_handler(uint64_t mcause, uint64_t mepc, uint64_t mtval)
 {
+        uart_puts("INTERRUPT");
 }
