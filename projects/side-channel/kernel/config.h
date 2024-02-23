039a7ff6483e69a83711ce79432e4334cadea26e
Henrik Karlsson <henrik10@kth.se>
2024-02-23 14:20:53 +0100

    Adds simple project demonstrating side-channel prevention

---
+++ projects/side-channel/kernel/config.h
@@ -0,0 +1,88 @@
+#include "kernel.h"
+#include "macro.h"
+
+/**
+ *
+ * typedef struct channel {
+ *         uint64_t *buf;
+ *         uint64_t size, head, tail;
+ * } channel_t;
+ *
+ * typedef struct zone {
+ *         uint64_t regs[32];
+ *         uint64_t pmpcfg;
+ *         uint64_t pmpaddr[8];
+ *         channel_t *chan_send;
+ *         uint64_t n_chan_send;
+ *         channel_t *chan_recv;
+ *         uint64_t n_chan_recv;
+ * } zone_t;
+ *
+ * typedef struct sched {
+ *         zone_t *zone;
+ *         uint64_t ticks;
+ * } sched_t;
+ */
+
+/****** IPC CONFIGURATIONS ******/
+
+static uint64_t chan_buf1[32];
+static channel_t chan1 = {
+	.buf = chan_buf1,
+	.size = ARRAY_SIZE(chan_buf1),
+	.head = 0,
+	.tail = 0,
+};
+
+static uint64_t chan_buf2[32];
+static channel_t chan2 = {
+	.buf = chan_buf2,
+	.size = ARRAY_SIZE(chan_buf2),
+	.head = 0,
+	.tail = 0,
+};
+
+/****** ZONE CONFIGURATIONS ******/
+static channel_t *zone1_send_chan[] = { &chan1 };
+static channel_t *zone1_recv_chan[] = { &chan2 };
+static zone_t zone1 = {
+        .regs = { 0x80010000 },
+        .pmp = {
+                .cfg = 0x1b1b1f,
+                .addr = {
+                PMP_NAPOT(0x80010000, 0x10000),
+                PMP_NAPOT(0x10000000, 0x20),
+                PMP_NAPOT(0x80030000, 0x10000),
+                },
+        },
+        .chan_send = zone1_send_chan,
+        .n_chan_send = ARRAY_SIZE(zone1_send_chan),
+        .chan_recv = zone1_recv_chan,
+        .n_chan_recv = ARRAY_SIZE(zone1_recv_chan),
+};
+
+static channel_t *zone2_send_chan[] = { &chan2 };
+static channel_t *zone2_recv_chan[] = { &chan1 };
+static zone_t zone2 = {
+        .regs = { 0x80020000 },
+        .pmp = {
+                .cfg = 0x1b1b1f,
+                .addr = {
+                PMP_NAPOT(0x80020000, 0x10000),
+                PMP_NAPOT(0x10000000, 0x20),
+                PMP_NAPOT(0x80030000, 0x10000),
+                },
+        },
+        .chan_send = zone2_send_chan,
+        .n_chan_send = ARRAY_SIZE(zone2_send_chan),
+        .chan_recv = zone2_recv_chan,
+        .n_chan_recv = ARRAY_SIZE(zone2_recv_chan),
+};
+
+/****** SCHEDULER CONFIGURATIONS ******/
+const sched_t schedule[] = {
+	{ &zone1, 500000, 1},
+	{ &zone2, 500000, 1},
+};
+
+const uint64_t yield_buffer = 10;
---
+++ projects/side-channel/zone1/main.c
@@ -0,0 +1,25 @@
+#include "api/openmz.h"
+#include "io/uart.h"
+
+volatile uint64_t *const shared_buffer = (uint64_t*)0x80030000;
+
+static inline uint64_t read_cycle(void) {
+        uint64_t res;
+        __asm__ volatile ("csrr %0,cycle" : "=r"(res));
+        return res;
+}
+
+void setup()
+{
+	uart_puts("setup trojan");
+}
+
+void loop()
+{
+        static int t = 0;
+        ecall_wfi();
+        if (t++ % 2 == 0) {
+                for (int i = 0; i < 128; ++i) 
+                        shared_buffer[i] += 0xdeadbeef;
+        }
+}
